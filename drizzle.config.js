4a99231e6c15c4556a2f0ce0d69ce3b8a0cae5bf
Ayush Sharaf <ayushsharaf3010@gmail.com>
2025-07-07 18:26:34 +0530

    fix security concerns

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type{import("drizzle-kit").Config} */
 export default {
-    schema: "./utils/schema.js",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: "postgresql://ai-mock-interview_owner:Tm5uq6bBXhZw@ep-gentle-union-a1bvb4rf.ap-southeast-1.aws.neon.tech/ai-mock-interview?sslmode=require",
-    },
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+  },
 };
